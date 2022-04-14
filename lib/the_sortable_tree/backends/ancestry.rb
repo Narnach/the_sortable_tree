de86fc5bb9466e2f73f4f605885205b29249e15e
Wes Oldenbeuving <wes@narnach.com>
2022-04-14 13:13:14 +0200

    Rails 6.0 compatibility: update_attributes! -> update!
    Using `ActiveRecord::Base#update_attributes!` is deprecated and will be removed in Rails 6.1. The new recommendation is to use `ActiveRecord::Base#update!` instead.

--- lib/the_sortable_tree/backends/ancestry.rb
+++ lib/the_sortable_tree/backends/ancestry.rb
@@ -6,17 +6,17 @@ module TheSortableTree
           self.the_sortable_tree_sort_order = 'ancestry, id'
 
           def move_to_child_of(parent)
-            self.update_attributes! parent_id: parent.id
+            self.update! parent_id: parent.id
           end
 
           # ancestry has no order (other than id), only care about nesting here
 
           def move_to_right_of(other)
-            self.update_attributes! parent_id: other.parent_id
+            self.update! parent_id: other.parent_id
           end
 
           def move_to_left_of(other)
-            self.update_attributes! parent_id: other.parent_id
+            self.update! parent_id: other.parent_id
           end
         end
       end
